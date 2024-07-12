275f7187b6fd198d6ae869f0d9674dcf109062d7
bhanudova02 <bhanudova02@gmail.com>
2024-07-12 12:46:23 +0530

    new ui changes added

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -4,8 +4,9 @@ import { HeaderProfile } from "./HeaderProfile";
 import { HeaderNotification } from "./HeaderNotification";
 import { SideNavComponent } from "./SideNavComponent";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 export function HeaderComponent() {
+    const pathName = usePathname();
     return (
         <header className="top-0 fixed w-full z-50 py-2 bg-white shadow-md shadow-gray-600">
             <div className="flex justify-between items-center container mx-auto">
@@ -19,12 +20,12 @@ export function HeaderComponent() {
                 </div>
                 <div className="lg:block hidden">
                     <ul className="flex items-center gap-6 font-medium">
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/ui_one">CompsOne</Link></li>
-                        <li><Link href="/ui_two">CompsTwo</Link></li>
-                        <li><Link href="/ui_three">CompsThree</Link></li>
-                        <li><Link href="/ui_four">CompsFour</Link></li>
-                        <li><Link href="/contact_us">Contact Us</Link></li>
+                        <li><Link className={`${pathName=="/"?'underline text-green-700 font-bold':''}`} href="/">Home</Link></li>
+                        <li><Link className={`${pathName=="/ui_one"?'underline text-green-700 font-bold':''}`} href="/ui_one">CompsOne</Link></li>
+                        <li><Link className={`${pathName=="/ui_two"?'underline text-green-700 font-bold':''}`} href="/ui_two">CompsTwo</Link></li>
+                        <li><Link className={`${pathName=="/ui_three"?'underline text-green-700 font-bold':''}`} href="/ui_three">CompsThree</Link></li>
+                        <li><Link className={`${pathName=="/ui_four"?'underline text-green-700 font-bold':''}`} href="/ui_four">CompsFour</Link></li>
+                        <li><Link className={`${pathName=="/contact_us"?'underline text-green-700 font-bold':''}`} href="/contact_us">Contact Us</Link></li>
                     </ul>
                 </div>
                 <div className="flex items-center gap-4">

293a2f041e927d65ec5855a49de5c3ca4c0a7e2f
bhanudova02 <bhanudova02@gmail.com>
2024-07-15 17:32:23 +0530

    new ui changes added

--- src/components/ComponentLeven.tsx
+++ src/components/ComponentLeven.tsx
@@ -52,10 +52,10 @@ export function ComponentLeven() {
                     <div className="relative w-full h-full">
                         <Image src={'/images-7.png'} width={500} height={500} alt="demo" className="w-full h-full object-cover" />
                         {/* Add overlay div */}
-                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"/>
                         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
                             <div className="w-full lg:w-[22rem]">
-                                <h3 className="text-white font-bold text-5xl">Infrastructure</h3>
+                                <h3 className="text-white font-bold text-3xl md:text-5xl">Infrastructure</h3>
                                 <p className="text-white font-semibold mt-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
                             </div>
                         </div>
@@ -66,8 +66,8 @@ export function ComponentLeven() {
 
                 <div className="w-full h-[400px] border bg-[url('/images-6.png')]">
                     <div className="bg-[rgba(0,0,0,0.8)] h-full w-full flex justify-center items-center">
-                        <div className="px-20    md:px-4 text-center">
-                            <h3 className="text-white font-bold text-5xl">Infrastructure</h3>
+                        <div className="px-4 text-center">
+                            <h3 className="text-white font-bold text-3xl md:text-5xl">Infrastructure</h3>
                             <p className="text-white font-semibold mt-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
                         </div>
                     </div>

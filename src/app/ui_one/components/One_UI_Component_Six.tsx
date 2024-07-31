596521b0fdc16c94967a861512ca8042a66c5745
bhanudova02 <bhanudova02@gmail.com>
2024-07-31 17:41:43 +0530

    new ui changes added

---
+++ src/app/ui_one/components/One_UI_Component_Six.tsx
@@ -0,0 +1,86 @@
+'use client'
+import { DialogBox } from "@/components/DialogBox";
+import Link from "next/link";
+
+const One_UI_Component_Six = () => {
+    const NAV_LINKS = [
+        { href: '/', key: 'home', label: 'Home' },
+        { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+        { href: '/', key: 'services', label: 'Services' },
+        { href: '/', key: 'pricing ', label: 'Pricing ' },
+        { href: '/', key: 'contact_us', label: 'Contact Us' },
+    ];
+
+
+    function handelCodeCopy() {
+        const codeToCopy = `
+    Array Data
+    ----------
+    const NAV_LINKS = [
+        { href: '/', key: 'home', label: 'Home' },
+        { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+        { href: '/', key: 'services', label: 'Services' },
+        { href: '/', key: 'pricing ', label: 'Pricing ' },
+        { href: '/', key: 'contact_us', label: 'Contact Us' },
+    ];
+
+    Code:
+    -----
+                        <ul className="hidden h-full gap-6 lg:flex">
+                            {NAV_LINKS.map((link) => (
+                                <Link href={link.href} key={link.key} className="text-green-500 cursor-pointer  transition-all hover:font-bold">
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </ul>
+
+    `;
+        navigator.clipboard.writeText(codeToCopy)
+    }
+
+
+    function handelCodeCopySecond() {
+        const codeToCopy = `
+    Image Source Link: 
+    ------------------
+    https://www.mediafire.com/folder/tatwoecdmiw2b/Component+Images
+
+    `;
+        navigator.clipboard.writeText(codeToCopy)
+    }
+
+
+
+
+
+    return (
+        <div className="container mx-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 ">
+                <div>
+                    <div className="bg-gray-100 rounded-md flex items-center justify-center p-4">
+                        <ul className="hidden h-full gap-6 lg:flex">
+                            {NAV_LINKS.map((link) => (
+                                <Link href={link.href} key={link.key} className="text-green-500 cursor-pointer transition-all hover:font-bold">
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
+                    <div onClick={handelCodeCopy} className="mt-4 opacity-60 hover:opacity-80 w-fit font-medium text-white rounded-md">
+                        <DialogBox />
+                    </div>
+                </div>
+                <div>
+                    <div className="bg-gray-100 rounded-md flex items-center justify-center p-4">
+                        <h2>Empty Place</h2>
+                    </div>
+                    <div onClick={handelCodeCopySecond} className="mt-4 opacity-60 hover:opacity-80 w-fit font-medium text-white rounded-md">
+                        <DialogBox />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default One_UI_Component_Six
--- src/app/ui_one/page.tsx
+++ src/app/ui_one/page.tsx
@@ -1,6 +1,7 @@
 import { One_UI_Component_Five } from "./components/One_UI_Component_Five";
 import { One_UI_Component_Four } from "./components/One_UI_Component_Four";
 import { One_UI_Component_One } from "./components/One_UI_Component_One";
+import One_UI_Component_Six from "./components/One_UI_Component_Six";
 import { One_UI_Component_Three } from "./components/One_UI_Component_Three";
 import { One_UI_Component_Two } from "./components/One_UI_Component_Two";
 
@@ -12,6 +13,7 @@ export default function page() {
             <One_UI_Component_Three/>
             <One_UI_Component_Four/>
             <One_UI_Component_Five/>
+            <One_UI_Component_Six/>
         </div>
     )
 }

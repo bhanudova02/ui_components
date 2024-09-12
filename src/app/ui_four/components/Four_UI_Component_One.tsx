3bf78b69302535dbe93d6caa713e3f161686b95c
bhanudova02 <bhanudova02@gmail.com>
2024-09-12 15:47:18 +0530

    new ui changes added

--- src/app/ui_four/components/Four_UI_Component_One.tsx
+++ src/app/ui_four/components/Four_UI_Component_One.tsx
@@ -12,16 +12,7 @@ const Four_UI_Component_One = () => {
 
   RenderCode:
   -----------
-  
-
-  `;
-    navigator.clipboard.writeText(codeToCopy)
-  }
-
-  return (
-    <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-      <h2 className='text-lg text-center font-semibold mb-3'>Layout Container</h2>
-      <div className='space-y-4'>
+    <div className='space-y-4'>
         {/* Layout One-1 */}
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className='bg-purple-600 text-white p-10 text-center'>
@@ -50,6 +41,51 @@ const Four_UI_Component_One = () => {
           </div>
         </div>
 
+        {/* Layout One-5 */}
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className='bg-lime-600 text-white p-10 text-center'>
+            Content goes here box-1
+            <div className="mx-auto max-w-3xl">
+              <div className='bg-green-400 text-white p-4 text-center'>
+                Content goes here box-2
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+    navigator.clipboard.writeText(codeToCopy)
+  }
+
+  return (
+    <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+      <h2 className='text-lg text-center font-semibold mb-3'>Layout Container</h2>
+      <div className='space-y-4'>
+        {/* Layout One-1 */}
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <div className='bg-purple-600 text-white p-10 text-center'>
+            Content goes here
+          </div>
+        </div>
+        {/* Layout One-2 */}
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className='bg-teal-600 text-white p-10 text-center'>
+            Content goes here
+          </div>
+        </div>
+        {/* Layout One-3 */}
+        <div className="container mx-auto sm:px-6 lg:px-8">
+          <div className='bg-yellow-600 text-white p-10 text-center'>
+            Content goes here
+          </div>
+        </div>
+
+        {/* Layout One-4 */}
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className='bg-sky-400 text-white p-10 text-center'>
+            Content goes here
+          </div>
+        </div>
         {/* Layout One-5 */}
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className='bg-lime-600 text-white p-10 text-center'>
